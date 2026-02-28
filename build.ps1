78325d69792959cf3e54566b6e65a2d6d5ad5410
CaptainBrando! <82909107+cptnbrando@users.noreply.github.com>
2026-02-28 17:53:45 -0600

    Modified build script for optional offline

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
 # To create a playbook for AME Beta v0.8.4
-# You zip up the contents of src, containing a playbook.conf and playbook.png as well as Configuration and Executables folder
-# Configuration folder requires a main.yml file outlining the playbook details
+# Zip up the contents of src, containing a playbook.conf and playbook.png as well as Configuration and Executables folders
+# Configuration folder must include a main.yml file outlining the playbook details
+#
 # The zip file must be encrypted with password "malte"
+#
 # Then, rename the .7z file to .apbx. Presto, you now have a valid playbook ready to rock
 # Though AME also checks for valid signing signatures, and if your playbook is forked from another playbook and changed
 # The app will load is as malicious. You may ignore this or change the id information
@@ -17,22 +19,37 @@ $finalFile = "revios.apbx"
 $password = "malte"
 $testExe = "./src/Executables/OfflineApps/vc_redist.x64.exe"
 
-# Delete old files
-if (Test-Path $tempZip) { Remove-Item $tempZip -Force }
-if (Test-Path $finalFile) { Remove-Item $finalFile -Force }
+Write-Host "Create offline .apbx? (y/n): " -ForegroundColor Cyan -NoNewline
+$answer = Read-Host
 
-# Check for offline exe files required for installation
-# Run install script if exe files are not present
-if (!(Test-Path $testExe)) {
-    Write-Host "Offline exe files not downloaded, running download script" -ForegroundColor Green
-    . ./src/Executables/OfflineApps/getapps.ps1
-} else {
-    Write-Host "Offline exe files found, skipping download script" -ForegroundColor Green
+# If creating an offline .apbx
+if ($answer -eq "y") {
+    # Check for offline exe files required for installation
+    # Run install script if exe files are not present
+    if (!(Test-Path $testExe)) {
+        Write-Host "Offline exe files not downloaded, running download script" -ForegroundColor Green
+        . ./src/Executables/OfflineApps/getapps.ps1
+    } else {
+        Write-Host "Offline exe files found, skipping download script" -ForegroundColor Green
+    }
 }
 
+# Delete old .7z file
+if (Test-Path $tempZip) { Remove-Item $tempZip -Force }
+
 # Archive and Encrypt
-Write-Host "Archiving and encrypting files..." -ForegroundColor Cyan
-& 7z a $tempZip $sourceDir "-p$password" -mhe=on
+if ($answer -eq "y") {
+    # OFFLINE BUILD: Include everything
+    Write-Host "Creating OFFLINE .apbx (including all files)..." -ForegroundColor Green
+    & 7z a $tempZip $sourceDir "-p$password" -mhe=on
+} else {
+    # ONLINE BUILD: Exclude the heavy offline installers
+    Write-Host "Creating ONLINE .apbx (excluding offline files)..." -ForegroundColor Yellow
+    & 7z a $tempZip $sourceDir "-p$password" -mhe=on "-x!Executables/OfflineApps"
+}
+
+# Delete old .apbx playbook file
+if (Test-Path $finalFile) { Remove-Item $finalFile -Force }
 
 # Rename to .apbx
 if (Test-Path $tempZip) {
